665bb163732abd6eae2da3e3881da584419e44aa
Tci Gravifer Fang <44160838+Gravifer@users.noreply.github.com>
2026-06-03 11:52:56 +0000

    chore: customize codespace welcome

---
+++ .devcontainer/welcome.sh
@@ -0,0 +1,4 @@
+echo "Welcome to the Dev Container! Customize it at .devcontainer/devcontainer.json" \
+| sudo tee /usr/local/etc/vscode-dev-containers/first-run-notice.txt > /dev/null
+echo "Welcome to the Codespace! Customize it at .devcontainer/devcontainer.json" \
+| sudo tee /workspaces/.codespaces/shared/first-run-notice.txt       > /dev/null
