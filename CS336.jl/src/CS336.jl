9a4ba51230c3ab7ccc577fcbe5f4167b5ce55645
Gravifer <44160838+Gravifer@users.noreply.github.com>
2026-07-22 20:21:18 +0800

    feat: scaffold CS336 Julia package

---
+++ CS336.jl/src/CS336.jl
@@ -0,0 +1,9 @@
+"""
+The Julia package for the CS336 parallel implementation.
+
+Assignment components are introduced incrementally after the package and parity
+harness are established.
+"""
+module CS336
+
+end
